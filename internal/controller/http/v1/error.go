ceea06993dce8e98df40e3c527684d02e4ca1a6b
SETTER <lphp@mail.ru>
2023-01-28 05:23:39 +0300

    iter3 #4 3

--- internal/controller/http/v1/error.go
+++ internal/controller/http/v1/error.go
@@ -1,13 +1,5 @@
 package v1
 
-import (
-	"github.com/go-chi/chi/v5"
-)
-
 type response struct {
 	Error string `json:"error" example:"message"`
-}
-
-func errorResponse(c *chi.Context, code int, msg string) {
-	return
 }
