025e44947ac59fca6047b7a6a69e53fe3cc39dcb
SETTER <lphp@mail.ru>
2023-02-04 01:51:09 +0300

    v 1

--- config/config.go
+++ config/config.go
@@ -29,7 +29,7 @@ var instance *Config
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	err := cleanenv.ReadConfig("../../config/config.yml", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
 	}
