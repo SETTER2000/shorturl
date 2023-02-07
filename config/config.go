c3261c90fa7060c1cd5c2542cb9e774495a3e1e4
SETTER <lphp@mail.ru>
2023-02-08 01:15:48 +0300

    fix 9

--- config/config.go
+++ config/config.go
@@ -21,8 +21,8 @@ type (
 		// BASE_URL - базовый адрес результирующего сокращённого URL
 		BaseURL string `env-required:"true" yaml:"base_url"`
 		// SERVER_ADDRESS - адрес запуска HTTP-сервера
-		ServerAddress string `env-required:"true" env:"SERVER_ADDRESS" yaml:"server_address,omitempty"`
-		ServerHost    string `env-required:"true" env:"SERVER_HOST" yaml:"server_host,omitempty"`
+		ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080" yaml:"server_address,omitempty"`
+		ServerHost    string `env:"SERVER_HOST" yaml:"server_host,omitempty"`
 	}
 
 	Log struct {
