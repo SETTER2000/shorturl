6429e21ebab13de4d37e2de638d1c1010511d4be
SETTER <lphp@mail.ru>
2023-02-08 16:55:23 +0300

    fix 17

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type (
 	HTTP struct {
 		Port string `env:"SERVER_PORT" envDefault:"8080"`
 		// BASE_URL - базовый адрес результирующего сокращённого URL
-		BaseURL string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+		BaseURL string `env:"BASE_URL" envDefault:"http://localhost"`
 		// SERVER_ADDRESS - адрес запуска HTTP-сервера
 		ServerAddress string `env:"SERVER_HOST" envDefault:"localhost"`
 	}
--- scripts/utils.go
+++ scripts/utils.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/SETTER2000/shorturl/config"
 	"hash/fnv"
-	"log"
 	"math/rand"
 	"os"
 	"time"
@@ -38,10 +37,16 @@ func UniqueString() string {
 }
 
 func GetHost(cfg config.HTTP, shorturl string) string {
+	// Формирует короткий URL
 	_, err := os.LookupEnv("BASE_URL")
+	// Если BASE_URL пустой или нет такой переменной окружения, то формирование url
+	// происходит из значений, которые стоят по умолчанию в конфиге (порт отдельно, хост отдельно)
 	if err {
-		return fmt.Sprintf("%s/%s", cfg.BaseURL, shorturl)
+		return fmt.Sprintf("%s:%s/%s", cfg.BaseURL, cfg.Port, shorturl)
 	}
-	log.Fatalf("GetHost BASE_URL error: %v", err)
-	return fmt.Sprintf("%s:%s/%s", cfg.BaseURL, cfg.Port, shorturl)
+	// .. в противном случаи т.к. BASE_URL имеет формат составной
+	// типа такого "http://$SERVER_HOST:$SERVER_PORT",
+	// соответственно он готов к использованию, возвращаем.
+	return fmt.Sprintf("%s/%s", cfg.BaseURL, shorturl)
+
 }
