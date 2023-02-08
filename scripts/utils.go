acf5b6260964c75f29e96eaef5860052ceac2dc5
SETTER <lphp@mail.ru>
2023-02-08 14:38:02 +0300

    fix 15

--- scripts/utils.go
+++ scripts/utils.go
@@ -36,7 +36,7 @@ func UniqueString() string {
 }
 
 func GetHost(cfg config.HTTP, shorturl string) string {
-	if cfg.Port != "" {
+	if cfg.Port != " " {
 		return fmt.Sprintf("%s:%s/%s", cfg.BaseURL, cfg.Port, shorturl)
 	}
 	return fmt.Sprintf("%s/%s", cfg.BaseURL, shorturl)
