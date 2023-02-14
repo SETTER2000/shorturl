6d2e1f4dacad049a5d0d862594127c3786940ad2
SETTER <lphp@mail.ru>
2023-02-14 14:26:48 +0300

    iter8 6

--- internal/controller/http/v1/shorturl.go
+++ internal/controller/http/v1/shorturl.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"github.com/SETTER2000/shorturl/config"
@@ -11,6 +14,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type shorturlRoutes struct {
@@ -48,11 +52,15 @@ func (r *shorturlRoutes) shortLink(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, fmt.Sprintf("%v", err), http.StatusBadRequest)
 		return
 	}
+	//b, err := Decompress(sh.URL)
+	//if err != nil {
+	//	fmt.Errorf("errrr %e", err)
+	//}
 	//fmt.Printf("Content--TYPE3 string(body): ind: %s\n", res.Header().Get("Content-Type"))
-	//fmt.Printf("Content--TYPE3 string(body): ind: %s\n", res.Header().Get("Content-Type"))
+	fmt.Printf("DetectContentType в GET: %s\n", http.DetectContentType(body))
 	//fmt.Printf("Content--sh.URL:  %s\n", sh.URL)
 	res.Header().Set("Content-Type", http.DetectContentType(body))
-	//res.Header().Add("Content-Encoding", "gzip")
+	res.Header().Add("Content-Encoding", "gzip")
 	//res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	//res.Header().Set("Content-Type", "application/gzip; charset=utf-8")
 	//res.Header().Add("Accept-Encoding", "application/json")
@@ -72,18 +80,31 @@ func (r *shorturlRoutes) shortLink(res http.ResponseWriter, req *http.Request) {
 // @Failure     500 {object} response
 // @Router      / [post]
 func (r *shorturlRoutes) longLink(res http.ResponseWriter, req *http.Request) {
+
+	if !strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
+		fmt.Println("если gzip не поддерживается, передаём управление дальше без изменений")
+	}
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
+	fmt.Printf("Accept-Encoding от клиента:%v\n", res.Header().Get("Accept-Encoding"))
+	fmt.Printf("Content-Encoding от клиента:%v\n", res.Header().Get("Content-Encoding"))
+	//b, err := Decompress(body)
+	//if err != nil {
+	//	fmt.Printf("errrr %e\n", err)
+	//}
+	//LengthHandle(res, req)
+	//fmt.Printf("Accept-Encoding от клиента:%v\n", res.Header().Get("Accept-Encoding"))
+	//fmt.Printf("Content--TYPE string(body): ind: %s\n", res.Header().Get("Content-Type"))
+	//fmt.Printf("Content--TYPE2 string(body): ind: %s\n", http.DetectContentType(body))
+	//fmt.Printf("POST string(bytes):%v\n", body)
+	//fmt.Printf("POST string(body):%s\n", body)
 
-	fmt.Printf("Content--TYPE string(body): ind: %s\n", res.Header().Get("Content-Type"))
-	fmt.Printf("Content--TYPE2 string(body): ind: %s\n", http.DetectContentType(body))
-	fmt.Printf("POST string(bytes):%v\n", body)
-	fmt.Printf("POST string(body):%s\n", body)
 	data := entity.Shorturl{}
 	data.URL = string(body)
+	fmt.Printf("SPLIT string(body):%s\n", strings.Split(data.URL, "\n"))
 	fmt.Printf("DATA  в POST::%s\n", data)
 	fmt.Printf("DATA URL в POST::%s\n", data.URL)
 	shorturl, err := r.s.LongLink(&data)
@@ -100,6 +121,74 @@ func (r *shorturlRoutes) longLink(res http.ResponseWriter, req *http.Request) {
 
 }
 
+// LengthHandle возвращает размер распакованных данных.
+func (r *shorturlRoutes) longLink3(res http.ResponseWriter, req *http.Request) {
+	// создаём *gzip.Reader, который будет читать тело запроса
+	// и распаковывать его
+	gz, err := gzip.NewReader(req.Body)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// не забывайте потом закрыть *gzip.Reader
+	defer gz.Close()
+
+	// при чтении вернётся распакованный слайс байт
+	body, err := io.ReadAll(gz)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//fmt.Fprintf(res, "Length: %d", len(body))
+
+	data := entity.Shorturl{}
+	data.URL = string(body)
+	fmt.Printf("SPLIT string(body):%s\n", strings.Split(data.URL, "\n"))
+	fmt.Printf("DATA  в POST::%s\n", data)
+	fmt.Printf("DATA URL в POST::%s\n", data.URL)
+	shorturl, err := r.s.LongLink(&data)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	d := scripts.GetHost(r.cfg, shorturl)
+	res.Header().Set("Content-Type", http.DetectContentType(body))
+
+	//res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(d))
+}
+func Decompress(data []byte) ([]byte, error) {
+	// переменная r будет читать входящие данные и распаковывать их
+	r := flate.NewReader(bytes.NewReader(data))
+	defer r.Close()
+
+	var b bytes.Buffer
+	// в переменную b записываются распакованные данные
+	_, err := b.ReadFrom(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed decompress data: %v", err)
+	}
+
+	return b.Bytes(), nil
+}
+func (r *shorturlRoutes) longLink2(res http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	body, err = Decompress(body)
+	if err != nil {
+		fmt.Printf("errrr %e", err)
+	}
+
+	fmt.Printf("Content==string(Content-Type):%s\n", res.Header().Get("Content-Type"))
+	fmt.Printf("Content== string(body):%s\n", http.DetectContentType(body))
+	fmt.Printf("POST== string(bytes):%v\n", body)
+	fmt.Printf("POST== string(body):%s\n", body)
+}
+
 type shorturlResponse struct {
 	URL string `json:"result"`
 }
@@ -114,7 +203,6 @@ type shorturlResponse struct {
 // @Failure     500 {object} response
 // @Router      /{shorten} [post]
 func (r *shorturlRoutes) shorten(res http.ResponseWriter, req *http.Request) {
-
 	data := entity.Shorturl{}
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
