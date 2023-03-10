c9528205babec9167f64659935df211cef77536b
SETTER <lphp@mail.ru>
2023-03-10 22:32:55 +0300

    iter14 24

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,10 +8,10 @@ import (
 )
 
 const (
-	_defaultReadTimeout     = 10 * time.Second
-	_defaultWriteTimeout    = 10 * time.Second
+	_defaultReadTimeout     = 15 * time.Second
+	_defaultWriteTimeout    = 15 * time.Second
 	_defaultAddr            = ":80"
-	_defaultShutdownTimeout = 3 * time.Second
+	_defaultShutdownTimeout = 15 * time.Second
 )
 
 // Server -.
