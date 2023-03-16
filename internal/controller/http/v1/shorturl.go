fc936c1f3e9d9ebf861393df4167f34ca8fd5f5d
SETTER <lphp@mail.ru>
2023-03-16 17:56:27 +0300

    iter14 69

--- internal/controller/http/v1/shorturl.go
+++ internal/controller/http/v1/shorturl.go
@@ -131,7 +131,6 @@ func (r *shorturlRoutes) longLink(res http.ResponseWriter, req *http.Request) {
 			shorturl = sh.Slug
 			res.Header().Set("Content-Type", http.DetectContentType(body))
 			res.WriteHeader(http.StatusConflict)
-
 		} else {
 			http.Error(res, err.Error(), http.StatusBadRequest)
 			return
